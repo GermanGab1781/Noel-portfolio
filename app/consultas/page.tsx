fa72a1fc8367ebb5dd4a7f5d37f9d30abd3a307a
German Gabriel <germangabriel1998@gmail.com>
2024-05-20 18:36:01 -0300

    Fixed height problem

--- app/consultas/page.tsx
+++ app/consultas/page.tsx
@@ -4,15 +4,18 @@ import { TbBrandWhatsapp } from "react-icons/tb";
 import { MdOutlineMailOutline } from "react-icons/md";
 export default function page() {
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="bg-blue-900 md:bg-[url('../public/images/FondoConsulta.png')] bg-[url('../public/images/FondoConsultaMobile.png')] md:bg-cover bg-[length:100%_100%] bg-no-repeat md:min-h-[94vh]">
+        <motion.div
+            initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}
+            className="bg-blue-900 md:bg-[url('../public/images/FondoConsulta.png')] bg-[url('../public/images/FondoConsultaMobile.png')] md:bg-cover bg-[length:100%_100%] bg-no-repeat md:min-h-[94vh]"
+        >
             <section className="w-full md:h-[94vh] h-full bg-gradient-to-br from-blue-900 to-transparent">
-                <h1 className="text-4xl text-center text-white pt-10 pb-5">Consultas virtuales</h1>
+                <h1 className="text-4xl text-center text-white mt-10 pb-5">Consultas virtuales</h1>
                 <div className="flex flex-col gap-y-2 text-xl text-gray-200 md:ml-2 md:px-0 px-2">
                     <p >&nbsp;Te brindaré la misma atención y calidad de servicio que recibirías en persona. Podremos discutir tus necesidades, resolver tus dudas y encontrar soluciones efectivas para cualquier situación que estés enfrentando. </p>
                     <p >&nbsp;Durante nuestra consulta, tendrás la oportunidad de hablar abiertamente sobre tus preocupaciones y recibir orientación personalizada.</p>
                     <p>&nbsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quas sequi rem minima ipsam reiciendis obcaecati ex quidem. Voluptas omnis nulla laboriosam veritatis, in dolore similique inventore aut natus adipisci repellat non provident, nisi quas harum eveniet explicabo quis dolorum numquam exercitationem id. Autem a quos placeat ad mollitia soluta sequi architecto tempora eveniet.</p>
-                    <div className="w-full md:pt-10">
-                        <p className="text-center md:pt-10 pt-5 font-bold text-white">Si estás listo/a para dar el siguiente paso, ¡estaré encantada de asistirte!</p>
+                    <div className="w-full md:mt-10">
+                        <p className="text-center md:mt-10 mt-5 font-bold text-white">Si estás listo/a para dar el siguiente paso, ¡estaré encantada de asistirte!</p>
                         <div className="flex place-content-center gap-x-10 text-[20vh] pb-5">
                             <a href="https://wa.me/+5492901304989?text=Hola, me gustaria tener una consulta virtual" target="_blank">
                                 <TbBrandWhatsapp className="text-green-500 hover:scale-105 cursor-pointer" />
